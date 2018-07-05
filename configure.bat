822367ba13f3e959d538eaa573c47c2b246fd700
Alexandre Plateau <plateau@irht2.cnrs.fr>
2018-07-05 16:38:58 +0200

    updating configure.bat

--- configure.bat
+++ configure.bat
@@ -2,7 +2,7 @@
 
 echo Checking if python3 is installed
 echo ================================
-where python 1>nul
+where py 1>nul
 if %ERRORLEVEL% neq 0 (
 	echo No python installation found
 	echo Go to https://www.python.org/downloads/release/python-370/
@@ -12,12 +12,12 @@ if %ERRORLEVEL% neq 0 (
 )
 set errorlevel=0
 
-py -c "import platform; print(1 if platform.python_version()[0]=='3' else 0)" > tmpfile
+py -c "import platform as p; v=p.python_version(); print(1 if v[0]=='3' and 54<=ord(v[1])<=57 else 0)" > tmpfile
 set /p py_version_check= < tmpfile
 del tmpfile
 if py_version_check == "0" (
 	echo Wrong version of Python installed
-	echo Consider installing a version of Python>=3 to continue
+	echo Consider installing a version of Python greater or equal to 3 to continue
 	exit
 ) else (
 	echo Python versions matching successfully
@@ -26,5 +26,5 @@ echo.
 
 echo Installing python modules
 echo =========================
-pip install elasticsearch==1.7.0
-pip install colorama
+py -m pip install elasticsearch==1.7.0
+py -m pip install colorama
